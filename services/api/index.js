dc1b7e5b520fccd6b2a8a412bcdf8962dc1e5fd3
kunal-mandalia <kunal.v.mandalia@gmail.com>
2018-06-04 23:45:57 +0100

    feat: enable cors

--- services/api/index.js
+++ services/api/index.js
@@ -1,8 +1,14 @@
 const app = require('express')()
 const port = process.env.PORT || 5000
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
+
 app.get('/sayhello', (req, res) => {
-  return res.status(200).json({
+  res.status(200).json({
     hello: 'world'
   })
 })
